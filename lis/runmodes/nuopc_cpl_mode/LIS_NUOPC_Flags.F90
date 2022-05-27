16dc2ac952cc1061e7c4348f1b902feb2fe78316
Dan Rosen <daniel.rosen@noaa.gov>
2022-05-27 11:10:34 -0600

    Update LIS NUOPC cap
    * Add field initialization options (default MISSING)

--- lis/runmodes/nuopc_cpl_mode/LIS_NUOPC_Flags.F90
+++ lis/runmodes/nuopc_cpl_mode/LIS_NUOPC_Flags.F90
@@ -44,15 +44,34 @@ module LIS_NUOPC_Flags
     MISSINGVAL_FAIL   = missingval_flag(1),  &
     MISSINGVAL_SKPCPY = missingval_flag(2)
 
+  type field_init_flag
+    sequence
+    private
+      integer :: opt
+  end type field_init_flag
+
+  type(field_init_flag), parameter ::       &
+    FLD_INIT_ERROR   = field_init_flag(-1), &
+    FLD_INIT_ZERO    = field_init_flag(0),  &
+    FLD_INIT_SKIP    = field_init_flag(1),  &
+    FLD_INIT_MISSING = field_init_flag(2),  &
+    FLD_INIT_IMPORT  = field_init_flag(3)
+
 !-----------------------------------------------------------------------------
 ! !PUBLIC MEMBER FUNCTIONS:
 !-----------------------------------------------------------------------------
 
   public missingval_flag
+  public field_init_flag
   public MISSINGVAL_ERROR
   public MISSINGVAL_IGNORE
   public MISSINGVAL_FAIL
   public MISSINGVAL_SKPCPY
+  public FLD_INIT_ERROR
+  public FLD_INIT_ZERO
+  public FLD_INIT_SKIP
+  public FLD_INIT_MISSING
+  public FLD_INIT_IMPORT
 
   public operator(==), assignment(=)
 
@@ -62,11 +81,14 @@ module LIS_NUOPC_Flags
 
   interface operator (==)
     module procedure missingval_eq
+    module procedure field_init_eq
   end interface
 
   interface assignment (=)
     module procedure missingval_toString
     module procedure missingval_frString
+    module procedure field_init_toString
+    module procedure field_init_frString
   end interface
 
   !-----------------------------------------------------------------------------
@@ -121,6 +143,61 @@ module LIS_NUOPC_Flags
       val = MISSINGVAL_ERROR
     endif
   end subroutine missingval_frString
+
+  !-----------------------------------------------------------------------------
+
+#undef METHOD
+#define METHOD "field_init_eq"
+  function field_init_eq(val1, val2)
+    logical field_init_eq
+    type(field_init_flag), intent(in) :: val1, val2
+    field_init_eq = (val1%opt == val2%opt)
+  end function field_init_eq
+
+  !-----------------------------------------------------------------------------
+
+#undef METHOD
+#define METHOD "field_init_toString"
+  subroutine field_init_toString(string, val)
+    character(len=*), intent(out) :: string
+    type(field_init_flag), intent(in) :: val
+    if (val == FLD_INIT_ZERO) then
+      write(string,'(a)') 'FLD_INIT_ZERO'
+    elseif (val == FLD_INIT_SKIP) then
+      write(string,'(a)') 'FLD_INIT_SKIP'
+    elseif (val == FLD_INIT_MISSING) then
+      write(string,'(a)') 'FLD_INIT_MISSING'
+    elseif (val == FLD_INIT_IMPORT) then
+      write(string,'(a)') 'FLD_INIT_IMPORT'
+    else
+      write(string,'(a)') 'FLD_INIT_ERROR'
+    endif
+  end subroutine field_init_toString
+
+  !-----------------------------------------------------------------------------
+
+#undef METHOD
+#define METHOD "field_init_frString"
+  subroutine field_init_frString(val, string)
+    type(field_init_flag), intent(out) :: val
+    character(len=*), intent(in) :: string
+    character(len=16) :: ustring
+    integer :: rc
+    ustring = ESMF_UtilStringUpperCase(string, rc=rc)
+    if (rc .ne. ESMF_SUCCESS) then
+      val = FLD_INIT_ERROR
+    elseif (ustring .eq. 'FLD_INIT_ZERO') then
+      val = FLD_INIT_ZERO
+    elseif (ustring .eq. 'FLD_INIT_SKIP') then
+      val = FLD_INIT_SKIP
+    elseif (ustring .eq. 'FLD_INIT_MISSING') then
+      val = FLD_INIT_MISSING
+    elseif (ustring .eq. 'FLD_INIT_IMPORT') then
+      val = FLD_INIT_IMPORT
+    else
+      val = FLD_INIT_ERROR
+    endif
+  end subroutine field_init_frString
 
   !-----------------------------------------------------------------------------
 
